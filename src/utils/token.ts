0d207fe5f95948f308c281aca19573c9acd49220
luojiongtian <luojiongtian@sina.com>
2023-03-07 16:09:04 +0800

    替换本地保存token的方式

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,17 +1,17 @@
 import config from '../config'
 
 export const setToken = (token: string):void => {
-  sessionStorage.setItem(config.TOKEN_KEY, token)
-  sessionStorage.setItem(config.LOGIN_LIMIT_KEY, (new Date().getTime() + config.LOGIN_LIMIT_TIME).toString())
+  localStorage.setItem(config.TOKEN_KEY, token)
+  localStorage.setItem(config.LOGIN_LIMIT_KEY, (new Date().getTime() + config.LOGIN_LIMIT_TIME).toString())
 }
 
-export const getToken: () => string = () => sessionStorage.getItem(config.TOKEN_KEY) || ''
+export const getToken: () => string = () => localStorage.getItem(config.TOKEN_KEY) || ''
 
-export const removeToken = ():void => sessionStorage.removeItem(config.TOKEN_KEY)
+export const removeToken = ():void => localStorage.removeItem(config.TOKEN_KEY)
 
 export const isLogin: () => boolean = () => {
   const token = getToken()
-  const limitTime = sessionStorage.getItem(config.LOGIN_LIMIT_KEY)
+  const limitTime = localStorage.getItem(config.LOGIN_LIMIT_KEY)
   if (token && limitTime && token !== '' && parseInt(limitTime) > new Date().getTime()) {
     return true
   }
